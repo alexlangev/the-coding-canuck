1dfa992e857c4dfe8ce3f3d729f8faf56fdda589
Alex Langevin <alexlangev@gmail.com>
2024-01-29 13:14:15 -0500

    removed page transitions

--- src/components/MatrixPaper/MatrixPaper.js
+++ src/components/MatrixPaper/MatrixPaper.js
@@ -1,21 +1,13 @@
-"use client";
 import React from "react";
 import styles from "./MatrixPaper.module.css";
-import { motion } from "framer-motion";
 
 function MatrixPaper({ as: Element = "article", children }) {
 	return (
-		<motion.div
-			initial={{ y: "+100vw" }}
-			animate={{ y: 0 }}
-			transition={{ duration: 1 }}
-		>
-			<div className={styles.wrapper}>
-				<Element className={styles.sheet}>
-					<div className={styles.perforated}>{children}</div>
-				</Element>
-			</div>
-		</motion.div>
+		<div className={styles.wrapper}>
+			<Element className={styles.sheet}>
+				<div className={styles.perforated}>{children}</div>
+			</Element>
+		</div>
 	);
 }
 
