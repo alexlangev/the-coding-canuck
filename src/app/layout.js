68164f210bc60735e8fb9bf27b47a00e91c8f08b
Alex Langevin <alexlangev@gmail.com>
2024-02-16 00:30:24 -0500

    new code snippet font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,62 +1,62 @@
-import { Red_Hat_Text, Red_Hat_Mono, Caveat } from "next/font/google";
+import { Red_Hat_Text, Caveat, JetBrains_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import clsx from "clsx";
 
 import {
-	BLOG_TITLE,
-	BLOG_DESCRIPTION,
-	DARK_TOKENS,
-	LIGHT_TOKENS,
+    BLOG_TITLE,
+    BLOG_DESCRIPTION,
+    DARK_TOKENS,
+    LIGHT_TOKENS,
 } from "@/constants";
 import { cookies } from "next/headers";
 
 const red_hat_text = Red_Hat_Text({
-	subsets: ["latin"],
-	display: "fallback",
-	weight: "variable",
-	variable: "--font-family",
+    subsets: ["latin"],
+    display: "fallback",
+    weight: "variable",
+    variable: "--font-family",
 });
 
-const red_hat_mono = Red_Hat_Mono({
-	subsets: ["latin"],
-	display: "fallback",
-	weight: "variable",
-	variable: "--font-family-code",
+const jet_brains_mono = JetBrains_Mono({
+    subsets: ["latin"],
+    display: "fallback",
+    weight: "variable",
+    variable: "--font-family-code",
 });
 
 const caveat = Caveat({
-	subsets: ["latin"],
-	weight: "variable",
-	display: "fallback",
-	variable: "--font-family-hand",
+    subsets: ["latin"],
+    weight: "variable",
+    display: "fallback",
+    variable: "--font-family-hand",
 });
 
 export const metadata = {
-	title: BLOG_TITLE,
-	description: BLOG_DESCRIPTION,
+    title: BLOG_TITLE,
+    description: BLOG_DESCRIPTION,
 };
 
 export default function RootLayout({ children }) {
-	const savedTheme = cookies().get("theme");
-	const theme = savedTheme?.value || "dark";
+    const savedTheme = cookies().get("theme");
+    const theme = savedTheme?.value || "dark";
 
-	return (
-		<html
-			lang="en"
-			style={theme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
-			className={clsx(
-				red_hat_text.variable,
-				red_hat_mono.variable,
-				caveat.variable
-			)}
-		>
-			<body>
-				<Header initialTheme={theme} />
-				<main>{children}</main>
-				<Footer />
-			</body>
-		</html>
-	);
+    return (
+        <html
+            lang="en"
+            style={theme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
+            className={clsx(
+                red_hat_text.variable,
+                jet_brains_mono.variable,
+                caveat.variable
+            )}
+        >
+            <body>
+                <Header initialTheme={theme} />
+                <main>{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
 }
